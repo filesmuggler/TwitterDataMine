ea64769ecd22099a46ed39d9cc9608098fccb3f8
Krzysztof Stężała <krzste09@outlook.com>
2018-03-18 11:51:52 +0100

    first gui interface

--- TwitterDataMine/TwitterDataMine.py
+++ TwitterDataMine/TwitterDataMine.py
@@ -2,16 +2,22 @@ import json
 import re
 import tweepy
 import operator
-
+import wx
 from collections import Counter
 from tweepy import OAuthHandler
+
+from BasicFrame import BasicFrame
 from displayUserData import displayUserData
 from printFromJson import printFromJson
 from configureAPI import configureAPI
 from collectTweets import collectTweets
 
+
 def main():
     
+    
+
+
     #configuring Twitter API
     api = configureAPI()
 
@@ -19,6 +25,10 @@ def main():
     target = 'chris_stezala09'
     number = 5
     collectTweets(api, target, number)
+
+    app = wx.App()
+    frm = BasicFrame(None,title="Hello Python!").Show()
+    app.MainLoop()
     
     
 if __name__=="__main__":
