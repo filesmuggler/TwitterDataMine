452073b06ee43db9346b0c9e022103a23bc1f56f
Krzysztof Stężała <krzste09@outlook.com>
2018-03-18 10:42:57 +0100

    configure API module

--- TwitterDataMine/TwitterDataMine.py
+++ TwitterDataMine/TwitterDataMine.py
@@ -4,34 +4,16 @@ import tweepy
 import operator
 
 from collections import Counter
-from tweepy import Stream
-from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
-from importKeys import importKeys
 from displayUserData import displayUserData
 from printFromJson import printFromJson
-from firstRun import firstRun
-
-import sys
+from configureAPI import configureAPI
 
 def main():
-    # get keys
-    path = "C:\\Users\\krzst\\keys.txt"
-    firstRun(path)
-
-    keys = []
-    keys = importKeys(path)
     
-    consumer_key = keys[0]
-    consumer_secret = keys[1]
-    access_token = keys[2]
-    access_secret = keys[3]
-        
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret)
+    #configuring Twitter API
+    api = configureAPI()
 
-    api = tweepy.API(auth) 
-    
     #targeting user
     target = 'chris_stezala09'
     number = 5
--- TwitterDataMine/importKeys.py
+++ TwitterDataMine/importKeys.py
@@ -1,6 +1,7 @@
 import json
 
 def importKeys(path):
+    # import keys from file specified in path
     keys = []
     with open(path, 'r') as f:
         for line in f:
